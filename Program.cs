57c2c7da5e25aca3533db72485f0676ea6ccd12f
NaumovaKate <naumova.katrine@gmail.com>
2022-12-19 18:35:19 +0300

    решена 1 задача

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int max = 0;
+int min = 0;
+
+Console.Write("Введите первое число: ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+
+if(Number1 > Number2)
+{
+    max = Number1;
+    min = Number2;
+}
+else
+{
+    max = Number1;
+    min = Number2;
+}
+Console.WriteLine("max = " + max + " и " + "min = " + min);
