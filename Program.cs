5081913273a34a8b6acf74e478322ec5b80eff86
NaumovaKate <naumova.katrine@gmail.com>
2022-12-19 19:49:29 +0300

    решены задачи 4 и 6, 8 задача не решена

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿int max = 0;
+﻿
+// Задача 2. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+int max = 0;
 int min = 0;
 
 Console.Write("Введите первое число: ");
@@ -18,3 +24,73 @@ else
     min = Number2;
 }
 Console.WriteLine("max = " + max + " и " + "min = " + min);
+
+//  Задача 4. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+int max1 = 0;
+
+Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число C: ");
+int C = Convert.ToInt32(Console.ReadLine());
+
+if(A > max1)
+{
+    max1 = A;
+}
+if(B > max1)
+{
+    max1 = B;    
+}
+if(C > max1)
+{
+    max1 = C;
+}
+Console.WriteLine("max1 = " + max1);
+
+/*
+Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет 
+*/
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int remDiv = number % 2;
+
+if(remDiv == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+/*
+Задача 8.Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+int N = 0;
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if(N > 2)
+{
+    
+}
